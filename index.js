d000cf4f1f3c118afe94347f0529a2950b0a0b69
laughter1998 <laughter98@hanmail.net>
2022-08-03 13:35:43 +0900

    아아

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const https = require('https');
 const AWS = require('aws-sdk');
 const mysql = require("./mysql.js");
-// 깃 테스트1 
+// 깃 테스트1 2
 
 
 exports.handler =  async function(event, context) {
